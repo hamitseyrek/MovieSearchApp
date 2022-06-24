dde7d4702b6b63c338c48e10d3c36d7f3931aa23
hamitseyrek <hamitseyrek@gmail.com>
2022-06-24 19:49:14 +0300

    Initial Commit

---
+++ MovieChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieChallenge
+//
+//  Created by Hamit Seyrek on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
